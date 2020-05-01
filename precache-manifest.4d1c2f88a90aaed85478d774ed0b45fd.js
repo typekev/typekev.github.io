ddeae0663ce3a3d960d5bac059070800e52908a9
Kevin Gonzalez <typekev@gmail.com>
2020-05-01 23:26:20 +0200

    add built site

---
+++ precache-manifest.4d1c2f88a90aaed85478d774ed0b45fd.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70f843b388271790af5ab810f3a0b9b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb42244fc650ac912a89",
+    "url": "/static/css/main.69f548e6.chunk.css"
+  },
+  {
+    "revision": "21c34b0c28031b63f594",
+    "url": "/static/js/0.c8dcdf15.chunk.js"
+  },
+  {
+    "revision": "84c8812385d9fdc66928",
+    "url": "/static/js/1.ee5febdf.chunk.js"
+  },
+  {
+    "revision": "4bb41b00cf274416589e",
+    "url": "/static/js/10.68b243b0.chunk.js"
+  },
+  {
+    "revision": "970092cb3ce73eccfc95",
+    "url": "/static/js/11.cf85ac3c.chunk.js"
+  },
+  {
+    "revision": "763a9f162fcaba856fbe",
+    "url": "/static/js/4.43c1621b.chunk.js"
+  },
+  {
+    "revision": "caaee4a72072ef016050dd7fd455a7d8",
+    "url": "/static/js/4.43c1621b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5f0ea56ad8e07a2f1fe",
+    "url": "/static/js/5.38ab4d29.chunk.js"
+  },
+  {
+    "revision": "e4a0d87becbcc6d09668875fe197b50f",
+    "url": "/static/js/5.38ab4d29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8a8e924457eaab9e460",
+    "url": "/static/js/6.e7eab110.chunk.js"
+  },
+  {
+    "revision": "afeb02850b81d4473654",
+    "url": "/static/js/7.b285e5ff.chunk.js"
+  },
+  {
+    "revision": "aaf75dba9ac44a1c3387",
+    "url": "/static/js/8.4393215b.chunk.js"
+  },
+  {
+    "revision": "a17e2cca05cbc43b500f",
+    "url": "/static/js/9.328df684.chunk.js"
+  },
+  {
+    "revision": "bb42244fc650ac912a89",
+    "url": "/static/js/main.b98f67bb.chunk.js"
+  },
+  {
+    "revision": "1efe203b8dd31566bc99",
+    "url": "/static/js/runtime-main.4a72d75b.js"
+  },
+  {
+    "revision": "1ad7a7411c2ca3bef4d075d36445302b",
+    "url": "/static/media/favicon.1ad7a741.ico"
+  },
+  {
+    "revision": "1c496f04591a45bad6915251cdfc98dc",
+    "url": "/static/media/favicon.1c496f04.ico"
+  },
+  {
+    "revision": "767e435f22df24eed03dc0579754df5b",
+    "url": "/static/media/site.767e435f.webmanifest"
+  },
+  {
+    "revision": "ae6f3331f66f8b82b9d2df8c6b3427d7",
+    "url": "/static/media/site.ae6f3331.webmanifest"
+  }
+]);
