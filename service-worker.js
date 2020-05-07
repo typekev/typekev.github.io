a902fa7d662349b1d22bd1c7c56ed188542ebf0a
typekev <19812642+typekev@users.noreply.github.com>
2020-05-07 18:28:44 +0000

    Deploying to master from  @ a1284cc032749b010684b6e5b0753cb664cfce57 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d592876b7b79bab69ef6163989f9e286.js"
+  "/precache-manifest.614c60f799454213d3caf012b9e51018.js"
 );
 
 self.addEventListener('message', (event) => {
